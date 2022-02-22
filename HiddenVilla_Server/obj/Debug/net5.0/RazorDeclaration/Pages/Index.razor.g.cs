c9a7785100e7f0941cab326acd99ad7084ca0137
NiranjanRK22 <niranjan.ramakrishna22@gmail.com>
2022-02-22 08:08:12 +0530

    components

--- HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,84 @@ namespace HiddenVilla_Server.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 1 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 2 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 3 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 4 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 5 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 6 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 7 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 8 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 9 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+#line 10 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "E:\Blazor\HiddenVilla\HiddenVilla_Server\Pages\Index.razor"
+#line 11 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+using HiddenVilla_Server.Pages.LearnBlazor.NewFolder;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\Pages\Index.razor"
 using Model;
 
 #line default
