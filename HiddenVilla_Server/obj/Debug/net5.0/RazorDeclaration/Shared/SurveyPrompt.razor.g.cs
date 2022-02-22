714dd8edcfc7ffc1e562a70b19e3460833761bdf
Niranjan Ramakrishna <Niranjanr@UNIFIEDIT.INDIA>
2022-02-22 19:01:07 +0530

    till database

--- HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ HiddenVilla_Server/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,78 +13,78 @@ namespace HiddenVilla_Server.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 1 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 2 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 3 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 4 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 5 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 6 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 7 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 8 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 9 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
+#line 10 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
 using HiddenVilla_Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\_Imports.razor"
-using HiddenVilla_Server.Pages.LearnBlazor.NewFolder;
+#line 11 "E:\Blazor\HiddenVilla\HiddenVilla_Server\_Imports.razor"
+using HiddenVilla_Server.Pages.LearnBlazor.LearnBlazorComponent;
 
 #line default
 #line hidden
@@ -97,7 +97,7 @@ using HiddenVilla_Server.Pages.LearnBlazor.NewFolder;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Niranjan\Dev\Blazor\HiddenVilla_Server\Shared\SurveyPrompt.razor"
+#line 12 "E:\Blazor\HiddenVilla\HiddenVilla_Server\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
